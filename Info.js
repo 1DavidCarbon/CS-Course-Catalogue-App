f6d11a0b68924cebf3fecbd44dc9552b3477773a
dennisr55523 <47605898+dennisr55523@users.noreply.github.com>
2019-02-13 20:43:52 -0800

    Info App
    oooof

---
+++ Info.js
@@ -0,0 +1,45 @@
+onEvent("creditButton", "click", function() {
+  setScreen("infoCreditScreen");
+});
+onEvent("infoButton1", "click", function() {
+  setScreen("infoScreen1");
+});
+onEvent("infoButton2", "click", function() {
+  setScreen("infoScreen2");
+});
+onEvent("infoButton3", "click", function() {
+  setScreen("infoScreen3");
+});
+onEvent("infoButton4", "click", function() {
+  setScreen("infoScreen4");
+});
+onEvent("infoButton5", "click", function() {
+  setScreen("infoScreen5");
+});
+onEvent("infoButton6", "click", function() {
+  setScreen("infoScreen6");
+});
+onEvent("infoScreen1Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("infoScreen2Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("backInfoButton", "click", function() {
+  setScreen("|=AppSelection=|");
+});
+onEvent("infoScreen3Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("infoScreen4Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("infoScreen5Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("infoScreen6Back", "click", function() {
+  setScreen("infoMainScreen");
+});
+onEvent("creditBackButton", "click", function() {
+  setScreen("infoMainScreen");
+});
