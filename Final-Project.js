86a8f9d28b4d5adb581af5663771c1d1bb57f030
DavidCarbon [REAL] <DavidLandaverdeJr@gmail.com>
2019-02-13 22:04:11 -0800

    Still waiting......
    Almost There! Just waiting for @AngelaRey33 last update

--- Final-Project.js
+++ Final-Project.js
@@ -714,7 +714,7 @@ var songR = "";
 var verb2R = "";
 var nounR = "";
 var adjR = "";
-var madliptextR = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verbR)+" everyday!\n 2. While in the shower, put on some ")+songR+"and make sure to "+verb2R+".\n 3. Eat two ")+nounR+" and quickly make your way out.\n 4. Make your way to ")+adjR+" school/work!";
+var madliptextR = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verbR)+" everyday!\n 2. While in the shower, put on some ")+songR+" and make sure to "+verb2R+".\n 3. Eat two ")+nounR+" and quickly make your way out.\n 4. Make your way to ")+adjR+" school/work!";
 
 onEvent("nextButton", "click", function(){
 setScreen("madlipScreen_APP");
@@ -723,7 +723,7 @@ var song = getText("songInput");
 var verb2 = getText("verbInput2");
 var noun = getText("nounInput");
 var adj = getText("adjInput");
-var madliptext = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verb)+" everyday!\n 2. While in the shower, put on some ")+song+"and make sure to "+verb2+".\n 3. Eat two ")+noun+" and quickly make your way out.\n 4. Make your way to ")+adj+" school/work!";
+var madliptext = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verb)+" everyday!\n 2. While in the shower, put on some ")+song+" and make sure to "+verb2+".\n 3. Eat two ")+noun+" and quickly make your way out.\n 4. Make your way to ")+adj+" school/work!";
 setText("madlip", madliptext);
 });
 onEvent("backButton", "click", playMadAgain);
