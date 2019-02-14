5ff4fd4d3b154fab159a02748a76718886cc9288
DavidCarbon [REAL] <DavidLandaverdeJr@gmail.com>
2019-02-13 21:12:37 -0800

    Updated with a Text Clear Fix
    I added a reset since it didn't clear the text if you were to add a new text

--- Madlib.js
+++ Madlib.js
@@ -1,10 +1,19 @@
+//==================Mad Lib APP======================//
 var verb;
 var song;
 var verb2;
 var noun;
 var adj;
+//Reset APP
+var verbR = "";
+var songR = "";
+var verb2R = "";
+var nounR = "";
+var adjR = "";
+var madliptextR = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verbR)+" everyday!\n 2. While in the shower, put on some ")+songR+"and make sure to "+verb2R+".\n 3. Eat two ")+nounR+" and quickly make your way out.\n 4. Make your way to ")+adjR+" school/work!";
+
 onEvent("nextButton", "click", function(){
-setScreen("madlipScreen");
+setScreen("madlipScreen_APP");
 var verb = getText("verbInput");
 var song = getText("songInput");
 var verb2 = getText("verbInput2");
@@ -13,6 +22,14 @@ var adj = getText("adjInput");
 var madliptext = (((("Waking up in the morning can be a difficult task!\n 1. Make sure to wake up on time and "+verb)+" everyday!\n 2. While in the shower, put on some ")+song+"and make sure to "+verb2+".\n 3. Eat two ")+noun+" and quickly make your way out.\n 4. Make your way to ")+adj+" school/work!";
 setText("madlip", madliptext);
 });
-onEvent("backButton", "click", function(){
-setScreen("madlibInput");
-});
+onEvent("backButton", "click", playMadAgain);
+function playMadAgain(){
+  setText("verbInput", verbR);
+  setText("songInput", songR);
+  setText("verbInput2", verb2R);
+  setText("nounInput", nounR);
+  setText("adjInput", adjR);
+  setText("madlip", madliptextR);
+  setScreen("madlibInput_APP");
+}
+//==================END Mad Lib APP======================//
